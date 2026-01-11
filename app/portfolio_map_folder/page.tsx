a747f64cc6e77bcd6d81c7c39bfe573eb126187f
Vonchie98 <adranfuentes546@gmail.com>
2026-01-10 19:28:41 -0600

    fix: update under construction message and image in portfolio and projects folders

--- app/portfolio_map_folder/page.tsx
+++ app/portfolio_map_folder/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function under_construction(){
     return (
-        <div className="bg-white/75 shadow-lg h-200 w-100">
-        <h1 className="absolute inset-0  flex items-center justify-center text-4xl font-extrabold text-white animate-pulse">
+        <div className="bg-white/75 shadow-lg h-200 w-100 flex items-center justify-center">
+        <h1 className="absolute inset-0 text-4xl font-extrabold text-white animate-pulse">
             Sorry this part isn't done yet
         </h1>
         
-        <Image src={'/under-construction.jpg'} alt='under construction' 
+        <Image src={'/under_construction.jpg'} alt='under construction' 
         width={800}
         height={800}
         className="w-full h-full object cover"/>
